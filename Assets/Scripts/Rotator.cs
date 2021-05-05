3f9f04ab2de67617dc9000ac073c433edd397226
picrossin <jadengoter@gmail.com>
2021-05-05 06:21:08 -0500

    Started adding new transition

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,14 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Rotator : MonoBehaviour
 {
-
-    [Header("Variables")]
+    [Header("Variables")] 
     [SerializeField] private float _rotateSpeed;
+    [SerializeField] private bool _randomizeOrientation;
 
-    void Update()
+    private void Start()
+    {
+        if (_randomizeOrientation)
+        {
+            transform.rotation = Quaternion.Euler(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+        }
+    }
+
+    private void Update()
     {
         transform.Rotate(new Vector3(0, _rotateSpeed, 0));
     }
